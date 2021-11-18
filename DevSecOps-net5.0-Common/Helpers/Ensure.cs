835c65a2d6e7372762df50d5e00435caef5dea00
Philipp Gaag <ph.gaag@gmail.com>
2021-11-18 16:36:38 +0100

    Added test and commons project

---
+++ DevSecOps-net5.0-Common/Helpers/Ensure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace DevSecOps_net5._0_Common.Helpers
+{
+    public static class Ensure
+    {
+        [DebuggerStepThrough]
+        public static T NotNull<T>(Expression<Func<T>> memberToCheckForNull) where T : class
+        {
+            if (memberToCheckForNull == null)
+            {
+                throw new NullReferenceException("No value given to check for null!");
+            }
+            var compiledExpression = memberToCheckForNull.Compile();
+            var result = compiledExpression(); 
+            if (compiledExpression() == null)
+            {
+                throw new ArgumentNullException((memberToCheckForNull.Body as MemberExpression)?.Member.Name);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DevSecOps_net5.0-Tests/WeatherForecastControllerTests.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevSecOps_net5._0;
+using DevSecOps_net5._0.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace DevSecOps_net5._0_Tests
+{
+    public class WeatherForecastControllerTests
+    {
+        private readonly Mock<ILogger<WeatherForecastController>> _logger = new Mock<ILogger<WeatherForecastController>>();
+        private readonly WeatherForecastController _weatherForecastController;
+
+        public WeatherForecastControllerTests()
+        {
+            _weatherForecastController = new WeatherForecastController(_logger.Object);
+        }
+
+        [Fact]
+        public void Constructor_All_Dependencies_Injected_Created_Successfully()
+        {
+            // Arrange & Act
+            var controller = new WeatherForecastController(_logger.Object);
+            
+            // Assert
+            Assert.NotNull(controller);
+        }
+        
+        [Fact]
+        public void Constructor_Logger_Null_Throws_Exception()
+        {
+            // Arrange & Act
+            var exception = Assert.Throws<ArgumentNullException>(() => new WeatherForecastController(null));
+            
+            // Assert
+            Assert.IsType<ArgumentNullException>(exception);
+        }
+
+        [Fact]
+        public void Get_Test()
+        {
+            // Arrange & Act
+            var result = _weatherForecastController.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsAssignableFrom<IEnumerable<WeatherForecast>>(result);
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
